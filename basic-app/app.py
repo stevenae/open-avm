7f95e59189d467d829982682c28b9a913e7a6dc0
stevenae <stevenalonzoellis@gmail.com>
2024-03-09 14:19:55 -0500

    switching to pandas

--- basic-app/app.py
+++ basic-app/app.py
@@ -3,81 +3,95 @@ from shiny.express import input, render, ui
 from shinywidgets import render_plotly
 import plotly.express as px
 import pandas as pd
-import polars as pl
-from itertools import islice
+import numpy as np
+import os
+
+ui.page_opts(fillable=True)
 
 # Read nowcast for visualization
 rank_cols = ['BATHRM','HF_BATHRM','BEDRM','GBA','YR_RMDL','EYB']
 nowcast_price_col = 'nowcast_prediction'
 address_col = 'ADDRESS'
-nowcast_select_cols = [*rank_cols,nowcast_price_col,address_col]
-preds = pl.read_csv('~/Documents/nowcast_predictions.csv',
-                    columns=nowcast_select_cols)
-
-preds = preds.with_columns(pl.col('BATHRM').add(pl.col('HF_BATHRM')))
-preds = preds.drop('HF_BATHRM')
+location_cols = ['LATITUDE','LONGITUDE']
+nowcast_select_cols = [*rank_cols,nowcast_price_col,address_col,*location_cols]
+preds_df = pd.read_csv(os.path.expanduser('~/Documents/nowcast_predictions.csv'),
+                       usecols=nowcast_select_cols)
+preds_df = preds_df.rename({'ADDRESS':'Address'}, axis='columns')
+address_col = 'Address'
+preds_df = preds_df.rename({'nowcast_prediction':'Nowcast'}, axis='columns')
+nowcast_price_col = 'Nowcast'
+pretty_names = {
+                'GBA':'Square Ft',
+                'BEDRM':'Bedrooms',
+                'BATHRM':'Bathrooms',
+                'EYB':'Year Built/Remodeled',
+            }
+preds_df[['BATHRM','HF_BATHRM']] = np.sum(preds_df[['BATHRM','HF_BATHRM']])
+preds_df = preds_df.drop('HF_BATHRM', axis=1)
 rank_cols.remove('HF_BATHRM')
-addresses = preds['ADDRESS']
+preds_df['EYB'] = np.max(preds_df[['EYB','YR_RMDL']])
+preds_df = preds_df.drop('YR_RMDL', axis=1)
+rank_cols.remove('YR_RMDL')
+addresses = preds_df[address_col]
 
-# Read backtest for error and comps
-prev_sale_price_col = 'PRICE'
-prev_sale_select_cols = [prev_sale_price_col,'SALEDATE','error','predictions']
-prev_sale_select_cols = prev_sale_select_cols.extend(address_col)
-errs = pl.read_csv('~/Documents/xgb_errors.csv',
-                    columns=prev_sale_select_cols)
-
-ui.page_opts(title="Penguins dashboard", fillable=True)
+ui.page_opts(title="RadarCh.art", fillable=True)
 
 with ui.sidebar():
-    ui.input_selectize(  
-        "selectize",  
-        "Type or select an address:",  
-        list(addresses.drop_nans().sample(10000).sort()),
+    ui.input_selectize(
+        "selectize",
+        "Type or select one or more addresses:",
+        list(addresses.sample(100).sort_values()),
         multiple=True,
         options = {'closeAfterSelect':True}
     )
+
     @render.data_frame
-    def summary_data():
-        selected_rows = preds.filter(pl.col('ADDRESS').is_in(input.selectize()))
-        price_offset_pct = .10
-        prediction_range_filter_bottom = selected_rows['nowcast_prediction'].min()*(1-price_offset_pct)
-        prediction_range_filter_top = selected_rows['nowcast_prediction'].max()*(1+price_offset_pct)
-        nowcast_prediction_offset_filter = pl.col('nowcast_prediction').is_between(prediction_range_filter_bottom,
-                                                                prediction_range_filter_top)
-        similarly_priced = preds.filter(nowcast_prediction_offset_filter)
-        # Add seleted rows to nearby homes to calculate ranks
-        similarly_priced = pl.concat([similarly_priced,selected_rows],how='vertical')
-        num_selected_rows = selected_rows.shape[0]
-        print(similarly_priced.tail(num_selected_rows))
-        similarly_priced = similarly_priced.with_columns((pl.col(rank_cols).rank() / pl.col(rank_cols).count()))
-        print(similarly_priced.tail(num_selected_rows))
-        return render.DataGrid(selected_rows,
-                               row_selection_mode="single",
-                               summary=False)
+    def nowcast_table():
+        nowcast = subset_nowcast()
+        nowcast = nowcast.rename(columns=pretty_names)
+        nowcast[nowcast_price_col] = nowcast[nowcast_price_col].div(1e3).map('${:,.0f}K'.format)
+        return render.DataGrid(nowcast,
+            row_selection_mode="single",
+            summary=False)
 
 @render.text
 def value():
-    return f"{input.selectize()}"
-
-
+    return "Percentiles of selected homes' attributes, compared to homes with similar (+/- 10%) price and location"
 
 @reactive.calc
-def df():
-    from palmerpenguins import load_penguins
-    return load_penguins()[input.var()]
+def subset_nowcast():
+    selected_rows = preds_df.loc[preds_df[address_col].isin(input.selectize()), :]
+    return selected_rows
 
-with ui.card(full_screen=True):
-    @render_plotly
-    def radar():
-        
-        df = pd.DataFrame(dict(
-            r=[1, 5, 2, 2, 3],
-            theta=['processing cost','mechanical properties','chemical stability',
-                   'thermal stability', 'device integration']))
-        fig = px.line_polar(df, r='r', theta='theta', line_close=True)
-        
-        return fig
+@reactive.calc
+def nowcast_similars():
+    nowcast_subset = subset_nowcast()
+    price_offset_pct = .10
+    price_range_filter_bottom = nowcast_subset[nowcast_price_col].min()*(1-price_offset_pct)
+    price_range_filter_top = nowcast_subset[nowcast_price_col].max()*(1+price_offset_pct)
+    nowcast_price_offset_filter = (nowcast_subset[nowcast_price_col].between(price_range_filter_bottom, price_range_filter_top))
+    similarly_priced = nowcast_subset.loc[nowcast_price_offset_filter, :]
 
+    location_offset_pct = .10
+    for location_col in location_cols:
+        location_range_filter_bottom = similarly_priced[location_col].min()*(1-location_offset_pct)
+        location_range_filter_top = similarly_priced[location_col].max()*(1+location_offset_pct)
+        location_col_offset_filter = (similarly_priced[location_col].between(location_range_filter_bottom, location_range_filter_top))
+        similarly_priced = similarly_priced.loc[location_col_offset_filter, :]
 
+    similarly_priced = pd.concat([similarly_priced,nowcast_subset],axis=0)
+    num_selected_rows = nowcast_subset.shape[0]
+    similarly_priced[rank_cols] = similarly_priced[rank_cols].rank(axis=0,ascending=False) / len(rank_cols)
+    radar_chart_vals = similarly_priced.tail(num_selected_rows)
+    return(radar_chart_vals)
 
-# fig.show()
+with ui.navset_pill(id="tab"):
+    with ui.nav_panel(title="Comparison",value='comparison_tab'):
+        @render_plotly
+        def radar():
+            similars = nowcast_similars()
+            similars = similars.drop([nowcast_price_col,*location_cols], axis=1)
+            similars = similars.rename(columns=pretty_names)
+            similars = similars.melt(id_vars=address_col,var_name='Attribute',value_name='Percentile')
+            fig = px.line_polar(similars, r='Percentile', color=address_col, theta='Attribute', line_close=True)
+            return fig
