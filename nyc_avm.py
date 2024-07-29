d2fcd6f847b9701071c7a0882b0357da30ef8990
stevenae <stevenalonzoellis@gmail.com>
2024-07-29 11:57:56 -0400

    finished investigation

--- nyc_avm.py
+++ nyc_avm.py
@@ -9,18 +9,25 @@ resi_fns = glob.glob(os.path.expanduser('~/Documents/Github/dcavm/nyc_data/*.xls
 dats = [pl.read_excel(resi_fn,engine="calamine",read_options={ "header_row": 4}) for resi_fn in resi_fns]
 dats = pl.concat(dats, how="vertical_relaxed")
 
+dats = dats.with_columns(block_pad = pl.col('BLOCK').cast(pl.String).str.zfill(5))
+dats = dats.with_columns(lot_pad = pl.col('LOT').cast(pl.String).str.zfill(4))
 dats = dats.with_columns(
     pl.concat_str(
         [
             pl.col("BOROUGH"),
-            pl.col("BLOCK"),
-            pl.col("LOT"),
+            pl.col("block_pad"),
+            pl.col("lot_pad"),
         ],
         separator="",
-    ).cast(pl.Int64).alias("bbl"),
+    ).cast(pl.Float64).alias("bbl"),
 )
 
-gis_fn = '~/Documents/Github/dcavm/nyc_data/Primary_Land_Use_Tax_Lot_Output__PLUTO_.csv'
+gis_fn = '~/Documents/Github/dcavm/nyc_data/pluto_24v2.csv'
 gis = pl.read_csv(gis_fn,infer_schema_length=10000)
+dats_anti = dats.join(gis,on='bbl',how='anti')
+dats["BOROUGH"].value_counts().join(
+    dats_anti["BOROUGH"].value_counts(), on="BOROUGH"
+).sort("BOROUGH")
+gis.filter((pl.col('borocode') == 5) & (pl.col('block') == 5742) & (pl.col('lot') == 1008))
 
 dats = dats.join(gis,on='bbl')
