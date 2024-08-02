c7f49500abcf2ec119b550eb7ae8f57afd04468d
stevenae <stevenalonzoellis@gmail.com>
2024-08-02 15:34:56 -0400

    init

---
+++ wake_nc_avm.py
@@ -0,0 +1,28 @@
+import polars as pl
+import xgboost as xgb
+import gnuplotlib as gp
+
+resi_fn = '~/Documents/Github/dcavm/wake_nc_data/Parcels.csv'
+dat = pl.read_csv(resi_fn,infer_schema_length=100000)
+
+dat = dat.filter(pl.col('TOTSALPRICE')>1e5)
+dat = dat.filter(pl.col('TOTSALPRICE')<2e6)
+
+dat = dat.with_columns(
+   pl.col("SALE_DATE").str.to_date("%Y/%m/%d %H:%M:%S+00")
+)
+
+gp.plot(dat['TOTSALPRICE'].to_numpy,{histogram:'freq'})
+
+
+
+xgb_data = dat.select(pl.col(['LATITUDE','LONGITUDE',
+           'BLDG_VAL','LAND_VAL','TOTAL_VALUE','YEAR_BUILT','DEISGN_STYL',
+                        'BEDRM','AYB','YR_RMDL','EYB','STORIES','GBA',
+                        'GRADE','CNDTN','EXTWALL','ROOF','INTWALL',
+                        'KITCHENS','FIREPLACES','LANDAREA',
+                        'SALEDATE',
+                        'NUM_UNITS','USECODE',
+                        'PRICE']))
+
+categories = ['DEISGN_STYL','ROOF','EXTWALL','INTWALL','AC','USECODE']
