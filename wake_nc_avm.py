9cd996f6b13f541e4264cfc703fa3e1170da2201
stevenae <stevenalonzoellis@gmail.com>
2024-08-07 12:49:39 -0400

    refactor wake

--- wake_nc_avm.py
+++ wake_nc_avm.py
@@ -1,28 +1,29 @@
 import polars as pl
 import xgboost as xgb
 
-resi_fn = '~/Documents/Github/dcavm/wake_nc_data/Parcels.csv'
-dat = pl.read_csv(resi_fn,infer_schema_length=100000)
+def prep_wake_nc():
+   resi_fn = '~/Documents/Github/dcavm/wake_nc_data/Parcels.csv'
+   dat = pl.read_csv(resi_fn,infer_schema_length=100000)
 
-dat = dat.filter(pl.col('TOTSALPRICE')>1e5)
-dat = dat.filter(pl.col('TOTSALPRICE')<2e6)
+   dat = dat.filter(pl.col('TOTSALPRICE')>1e5)
+   dat = dat.filter(pl.col('TOTSALPRICE')<2e6)
 
-dat = dat.with_columns(
-   pl.col("SALE_DATE").str.to_date("%Y/%m/%d %H:%M:%S+00")
-)
+   dat = dat.with_columns(
+      pl.col("SALE_DATE").str.to_date("%Y/%m/%d %H:%M:%S+00")
+   )
 
-gis_fn = '~/Documents/Github/dcavm/wake_nc_data/Parcel_Address_Points.csv'
-gis = pl.read_csv(gis_fn)
+   gis_fn = '~/Documents/Github/dcavm/wake_nc_data/Parcel_Address_Points.csv'
+   gis = pl.read_csv(gis_fn)
 
-dat = dat.join(gis, on='PIN_NUM')
+   dat = dat.join(gis, on='PIN_NUM')
 
-xgb_data = dat.select(pl.col(['LATITUDE','LONGITUDE',
-           'BLDG_VAL','LAND_VAL','TOTAL_VALUE','YEAR_BUILT','DEISGN_STYL',
-                        'BEDRM','AYB','YR_RMDL','EYB','STORIES','GBA',
-                        'GRADE','CNDTN','EXTWALL','ROOF','INTWALL',
-                        'KITCHENS','FIREPLACES','LANDAREA',
-                        'SALEDATE',
-                        'NUM_UNITS','USECODE',
-                        'PRICE']))
+   xgb_data = dat.select(pl.col(['LATITUDE','LONGITUDE',
+            'BLDG_VAL','LAND_VAL','TOTAL_VALUE','YEAR_BUILT','DEISGN_STYL',
+                           'BEDRM','AYB','YR_RMDL','EYB','STORIES','GBA',
+                           'GRADE','CNDTN','EXTWALL','ROOF','INTWALL',
+                           'KITCHENS','FIREPLACES','LANDAREA',
+                           'SALEDATE',
+                           'NUM_UNITS','USECODE',
+                           'PRICE']))
 
-categories = ['DEISGN_STYL','ROOF','EXTWALL','INTWALL','AC','USECODE']
+   categories = ['DEISGN_STYL','ROOF','EXTWALL','INTWALL','AC','USECODE']
